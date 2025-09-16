625d5bf1728931d8dc0e50e9808fb5b636c198d3
felix5572 <yfb222333@gmail.com>
2025-09-16 10:47:39 -0500

    fix: update back_init_entrypoint.sh to use login shell and print environment variables before starting the backend

--- backend/back_init_entrypoint.sh
+++ backend/back_init_entrypoint.sh
@@ -2,7 +2,7 @@
 
 # run Open WebUI and initialize (not block the front)
 echo "Starting Open WebUI and initialization in background..."
-nohup bash -c 'cd /app/backend && source start.sh' > /var/log/start.log 2>&1 &
+nohup bash  -l -c 'cd /app/backend && env && source ./start.sh' > /var/log/start.log 2>&1 &
 
 # run Jupyter Lab (run CMD)
 exec "$@"
